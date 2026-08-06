fec134a5e659dd5ec0bedaf921bd336839810b0c
muditsingh-sudo <mudit.singh@joshtechnologygroup.com>
2026-08-06 22:52:15 +0530

    [MK_A1_02]: CodeRabbit fixes

--- src/script/index.js
+++ src/script/index.js
@@ -5,6 +5,9 @@ const menuDropDownContainer = document.querySelector('.navbar__menu-dropdown');
 let isMenuActive = false;
 
 const handleMenuClick = (event) => {
+    /**
+     * This is used to handle the opening and closing of the menu dropdown when the menu icon is clicked. Also prevents propagation to outside elements.
+     */
     event.stopPropagation();
     if (!isMenuActive) {
         isMenuActive = true;
@@ -26,6 +29,9 @@ const linkItem = document.querySelector('.navbar__link-item');
 
 let isLinkOpen = false;
 const handleLinkClick = (event) => {
+    /**
+     * This is used to handle the opening and closing of the links dropdown when the link is clicked. Also prevents propagation to outside elements.
+     */
     event.stopPropagation();
 
     if (!isLinkOpen) {
