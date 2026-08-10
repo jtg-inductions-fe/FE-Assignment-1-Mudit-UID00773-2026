23788d278e3b83f1fb10ad53b0a493639b115352
muditsingh-sudo <mudit.singh@joshtechnologygroup.com>
2026-08-10 12:45:50 +0530

    [MK_A1_02]: Navbar (#2)
    * [MK_A1_01]: Base Setup
    
    * [MK_A1_01]: PR fixes
    
    * [MK_A1_01]: PR fixes 2
    
    * [MK_A1_01]: Image fix
    
    * [MK_A1_02]: Navbar
    
    * [MK_A1_02]: CodeRabbit fixes
    
    * [MK_A1_02]: PR Fixes

---
+++ src/script/index.js
@@ -0,0 +1,46 @@
+const menuIconContainer = document.querySelector('.js-menu-toggle');
+const menuDropDownContainer = document.querySelector('.js-menu-dropdown');
+let isMenuActive = false;
+
+/**
+ * Opens or closes the drop-down menu when the menu button is clicked.
+ * It also stops the click from triggering other actions elsewhere on the page.
+ */
+const handleMenuClick = (event) => {
+    event.stopPropagation();
+    if (!isMenuActive) {
+        isMenuActive = true;
+        menuIconContainer.classList.add('navbar__menu-icon-container--clicked');
+        menuDropDownContainer.classList.remove('u-hidden');
+    } else {
+        isMenuActive = false;
+        menuIconContainer.classList.remove(
+            'navbar__menu-icon-container--clicked',
+        );
+        menuDropDownContainer.classList.add('u-hidden');
+    }
+};
+
+menuIconContainer.addEventListener('click', handleMenuClick);
+
+const linkDropdown = document.querySelector('.js-link-dropdown');
+const linkItem = document.querySelector('.js-link-item');
+
+let isLinkOpen = false;
+/**
+ * Opens or closes the links drop-down menu when the link is clicked.
+ * It also stops the click from triggering other actions elsewhere on the page.
+ */
+const handleLinkClick = (event) => {
+    event.stopPropagation();
+
+    if (!isLinkOpen) {
+        linkDropdown.classList.remove('u-hidden');
+    } else {
+        linkDropdown.classList.add('u-hidden');
+    }
+
+    isLinkOpen = !isLinkOpen;
+};
+
+linkItem.addEventListener('click', handleLinkClick);
